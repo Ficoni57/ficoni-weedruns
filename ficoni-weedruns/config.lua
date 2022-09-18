ecfe4a5cf36888dc21336d966f00597666edfd34
Ficoni <104675574+Ficoni57@users.noreply.github.com>
2022-09-18 22:49:34 +0200

    Update config.lua

--- ficoni-weedruns/config.lua
+++ ficoni-weedruns/config.lua
@@ -4,10 +4,10 @@ Config.WashMoney = true
 
 Config.MarkedBills = 100
 
-Config.BandMinPayout = 80
-Config.BandMaxPayout = 100
-Config.BandNotesChance = 15
-
 Config.RollMinPayout = 20
 Config.RollMaxPayout = 25
 Config.CashRollChance = 10
+
+Config.BandMinPayout = 80
+Config.BandMaxPayout = 100
+Config.BandNotesChance = 15
