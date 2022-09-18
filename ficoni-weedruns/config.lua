1dcccf8b9896d56aed78ac6275b7a7ef06de6f71
Ficoni <104675574+Ficoni57@users.noreply.github.com>
2022-09-18 20:55:02 +0200

    Add files via upload

---
+++ ficoni-weedruns/config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+Config.WashMoney = true 
+
+Config.MarkedBills = 100
+
+Config.BandMinPayout = 80
+Config.BandMaxPayout = 100
+Config.BandNotesChance = 15
+
+Config.RollMinPayout = 20
+Config.RollMaxPayout = 25
+Config.CashRollChance = 10
---
+++ ficoni-weedruns/server/main.lua
@@ -0,0 +1,51 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+local Cooldown = false
+local checkpackage = true
+local packagerecieved = false
+players = {}
+entities = {}
+
+RegisterServerEvent('givememoneybro', function()
+	local Player = QBCore.Functions.GetPlayer(source)
+	local ply = QBCore.Functions.GetPlayer(source)
+	local hasMarkedBills = Player.Functions.GetItemByName("markedbills")
+	local hasBandNotes = Player.Functions.GetItemByName("bands")
+	local hasCashRolls = Player.Functions.GetItemByName("rolls")
+
+	local rollchance = math.random(1, 10)
+	Player.Functions.AddMoney('cash', math.random(3950, 5000))
+		if Config.WashMoney then
+ 
+			if rollchance < Config.MarkedBills then
+				if hasMarkedBills then
+					worth = Player.Functions.GetItemByName('markedbills').info.worth
+					markedbillsworth = math.random(Player.Functions.GetItemByName("markedbills").amount)
+					Player.Functions.AddMoney('cash', markedbillsworth*worth)
+
+					Player.Functions.RemoveItem("markedbills", markedbillsworth)
+					TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items["markedbills"], "remove")
+				end
+			end
+
+			if rollchance < Config.CashRollChance then
+				if hasCashRolls then
+					cashrollworth = math.random(Player.Functions.GetItemByName("rolls").amount)
+					Player.Functions.AddMoney('cash', cashrollworth*math.random(Config.RollMinPayout, Config.RollMaxPayout))
+
+					Player.Functions.RemoveItem("rolls", cashrollworth)
+					TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items["rolls"], "remove")
+				end
+			end
+
+			if rollchance < Config.BandNotesChance then
+				if hasBandNotes then
+					bandnotesworth = math.random(Player.Functions.GetItemByName("bands").amount)
+					Player.Functions.AddMoney('cash', bandnotesworth*math.random(Config.BandMinPayout, Config.BandMaxPayout))
+
+					Player.Functions.RemoveItem("bands", bandnotesworth)
+					TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items["bands"], "remove")
+			    end
+	        end
+	   end
+end)
